17a5e06dbc001a46a2eb9f43c2d279d2986b456c
William Keller <wjkeller@ucdavis.edu>
2011-10-08 16:18:35 -0700

    Added const to the other element of the command struct to make the string valid

--- hw2/mscp-1.4/mscp.cpp
+++ hw2/mscp-1.4/mscp.cpp
@@ -174,7 +174,7 @@ enum { RANK_1, RANK_2, RANK_3, RANK_4, RANK_5, RANK_6, RANK_7, RANK_8 };
 struct command {
         const char *name;
         void (*cmd)(char*);
-        char *help;
+        const char *help;
 };
 
 /* attacks */
